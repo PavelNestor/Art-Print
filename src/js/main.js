710fc106c2de05e22b260ea2508f68ab3c88dd24
PavelNestor <pavelnesterets82@gmail.com>
2019-09-10 11:56:38 +0300

    start

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-(function() {
-  var menuImg = document.getElementById("menu-image");
-  var menuClose = document.getElementById("menu-close");
-  var menuContent = document.getElementById("menu-content");
+// (function() {
+//   var menuImg = document.getElementById("menu-image");
+//   var menuClose = document.getElementById("menu-close");
+//   var menuContent = document.getElementById("menu-content");
 
-  const onToogleMenu = () => {
-    menuContent.classList.toggle("menu-show");
-  };
+//   const onToogleMenu = () => {
+//     menuContent.classList.toggle("menu-show");
+//   };
 
-  menuImg.addEventListener("click", onToogleMenu);
-  menuImg.addEventListener("touch", onToogleMenu);
-  menuClose.addEventListener("click", onToogleMenu);
-})();
+//   menuImg.addEventListener("click", onToogleMenu);
+//   menuImg.addEventListener("touch", onToogleMenu);
+//   menuClose.addEventListener("click", onToogleMenu);
+// })();
