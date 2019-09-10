68ab0862513175cfc9f1d5b2d9952c269195aae0
PavelNestor <pavelnesterets82@gmail.com>
2019-09-10 13:41:57 +0300

    add slider-template (#1)

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,38 @@
 "use strict";
 
-// (function() {
-//   var menuImg = document.getElementById("menu-image");
-//   var menuClose = document.getElementById("menu-close");
-//   var menuContent = document.getElementById("menu-content");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-//   const onToogleMenu = () => {
-//     menuContent.classList.toggle("menu-show");
-//   };
+const slides = $$('.slides');
+const prevSlide = $('#prev-slide');
+const nextSlide = $('#next-slide');
 
-//   menuImg.addEventListener("click", onToogleMenu);
-//   menuImg.addEventListener("touch", onToogleMenu);
-//   menuClose.addEventListener("click", onToogleMenu);
-// })();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlides() {
+  showSlides((slideIndex += 1));
+}
+
+function prewSlides() {
+  showSlides((slideIndex += -1));
+}
+
+function showSlides(index) {
+  console.log('index', index);
+  console.log('slides', slides.length);
+  
+  if (index > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (index < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach(slide => { slide.style.display = 'none' });
+  slides[slideIndex - 1].style.display = 'block';
+}
+
+prevSlide.addEventListener('click', prewSlides);
+nextSlide.addEventListener('click', nextSlides);
