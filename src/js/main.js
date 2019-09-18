4e64951ce149c8e4c78130ff3a947d207bb3fad8
AndrewKirichenko <a.kirichenko.developer@gmail.com>
2019-09-18 10:26:09 +0300

    update nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,38 @@
-"use strict";
+// "use strict";
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
 
-const slides = $$('.slides');
-const prevSlide = $('#prev-slide');
-const nextSlide = $('#next-slide');
+// const slides = $$('.slides');
+// const prevSlide = $('#prev-slide');
+// const nextSlide = $('#next-slide');
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-function nextSlides() {
-  showSlides((slideIndex += 1));
-}
+// function nextSlides() {
+//   showSlides((slideIndex += 1));
+// }
 
-function prewSlides() {
-  showSlides((slideIndex += -1));
-}
+// function prewSlides() {
+//   showSlides((slideIndex += -1));
+// }
 
-function showSlides(index) {
-  console.log('index', index);
-  console.log('slides', slides.length);
+// function showSlides(index) {
+//   console.log('index', index);
+//   console.log('slides', slides.length);
   
-  if (index > slides.length) {
-    slideIndex = 1;
-  }
+//   if (index > slides.length) {
+//     slideIndex = 1;
+//   }
 
-  if (index < 1) {
-    slideIndex = slides.length;
-  }
+//   if (index < 1) {
+//     slideIndex = slides.length;
+//   }
 
-  slides.forEach(slide => { slide.style.display = 'none' });
-  slides[slideIndex - 1].style.display = 'block';
-}
+//   slides.forEach(slide => { slide.style.display = 'none' });
+//   slides[slideIndex - 1].style.display = 'block';
+// }
 
-prevSlide.addEventListener('click', prewSlides);
-nextSlide.addEventListener('click', nextSlides);
+// prevSlide.addEventListener('click', prewSlides);
+// nextSlide.addEventListener('click', nextSlides);
