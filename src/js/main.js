f3f5d7dad504289e03f9e4c1792e1014bf557255
AndrewKirichenko <a.kirichenko.developer@gmail.com>
2019-09-19 18:01:49 +0300

    add second section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
-// "use strict";
+"use strict";
 
+const numCols = 3;
+const colHeights = Array(numCols).fill(0);
+const container = document.getElementById('masonry-with-columns');
+Array.from(container.children).forEach((child, i) => {
+  const order = i % numCols;
+  child.style.order = order;
+  colHeights[order] += parseFloat(child.clientHeight);
+})
+container.style.height = Math.max(...colHeights) + 'px';
 // const $ = document.querySelector.bind(document);
 // const $$ = document.querySelectorAll.bind(document);
 
